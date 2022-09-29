bb1db284995f329fcdb5d8c8a2434deb322e3d23
gaddechiranjeevi <gaddechiru@gmail.com>
2022-09-29 15:08:13 +0530

    object in the local storage

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-let email,phone,timeforcall;
-let name=document.getElementById("name").value;
- email=document.getElementById("email").value;
- phone=document.getElementById("phone").value;
- timeforcall=document.getElementById("time for call").value;
- localStorage.setItem("name",name);
- localStorage.setItem("email",email);
- localStorage.setItem("phone",phone);
- localStorage.setItem("time for call",timeforcall);
+let myObj = {
+    name : "kiran",
+    age : 25
+}
+localStorage.setItem("myObj", myObj);
+console.log(localStorage);
+
+let myObj_serialized = JSON.stringify(myObj);
+//console.log(myObj_serialized);
+
+localStorage.setItem("myObj", myObj_serialized);
+//console.log(localStorage);
+
+let myObj_deserialized = JSON.parse(localStorage.getItem("myObj"));
+console.log(myObj_deserialized);
